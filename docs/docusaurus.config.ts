38f9ad526bdb85f02c6064b0436cf99ade44d381
Dylan Vorster <dylanvorster.dv@gmail.com>
2025-07-28 16:07:20 -0600

    docs

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,112 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+import { packages } from './utils/packages';
+import { generateTSDocPlugin } from './utils/packageUtils';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Reactor Docs',
+  tagline: 'Ambitious app building framework',
+  favicon: 'img/favicon.ico',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'JourneyApps Labs', // Usually your GitHub org/user name.
+  projectName: 'Reactor', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en']
+  },
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          routeBasePath: '/docs',
+          sidebarPath: './sidebars.ts',
+          path: 'docs'
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn'
+        },
+        theme: {
+          customCss: './src/css/custom.css'
+        }
+      } satisfies Preset.Options
+    ]
+  ],
+  plugins: packages.map((p) => generateTSDocPlugin(p)),
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/labs.png',
+    navbar: {
+      title: 'Reactor',
+      logo: {
+        alt: 'Labs Logo',
+        src: 'img/labs.png'
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'mainSidebar',
+          position: 'left',
+          label: 'Getting started'
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'docsSidebar',
+          position: 'left',
+          label: 'TSDoc'
+        },
+        { to: '/blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/journeyapps-labs/reactor',
+          label: 'GitHub',
+          position: 'right'
+        }
+      ]
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} JourneyApps`
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula
+    }
+  } satisfies Preset.ThemeConfig
+};
+
+export default config;
---
+++ docs/utils/packages.ts
@@ -0,0 +1,19 @@
+import { Package } from './packageUtils';
+
+export const packages = [
+  {
+    dir: '../modules/module-reactor',
+    id: 'module-reactor',
+    name: 'Module Reactor'
+  },
+  {
+    dir: '../modules/module-editor',
+    id: 'module-editor',
+    name: 'Module Editor'
+  },
+  {
+    dir: '../libs/lib-reactor-utils',
+    id: 'lib-reactor-utils',
+    name: 'Lib Reactor Utils'
+  }
+] as Package[];
