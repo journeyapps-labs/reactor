d77282b393d132dd16a91cb41e73b58ce9fed230
Dylan Vorster <dylanvorster.dv@gmail.com>
2025-07-29 13:45:42 -0600

    url was wrong

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://journeyapps-labs.github.io/reactor',
+  url: 'https://journeyapps-labs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/reactor/',
