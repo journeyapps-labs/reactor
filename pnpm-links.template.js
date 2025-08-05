b802bec6d189b8cd23bb467a001c50420ed8114d
Dylan Vorster <dylanvorster.dv@gmail.com>
2025-08-05 15:40:50 -0600

    add links script

---
+++ pnpm-links.template.js
@@ -0,0 +1,5 @@
+// Can use this file to replace links to packages such as the react-workspaces packages for local development
+module.exports = {
+  // "@projectstorm/react-workspaces-behavior-divider-dropzone": "../../os/react-workspaces/packages/behavior-divider-dropzone",
+  //...
+}
